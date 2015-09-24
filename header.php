c56c9362d0216f08faa9828a43606ec5759a1cbc
Lena Plaksina <lena.plaksina@gmail.com>
2015-09-25 11:52:30 +1200

    Add triangles as after elements for active and hovering menu items with media queries

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'lena_fttl' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'lena_fttl' ); ?></button>
 			<div class="top-nav">
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 			</div>
