34ffaac650c90b22ac7012a414f59bfcd266d9d1
Lena Plaksina <lena.plaksina@gmail.com>
2015-09-25 11:19:46 +1200

    Fix mobile nav toggle button

--- header.php
+++ header.php
@@ -36,7 +36,9 @@
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'lena_fttl' ); ?></button>
-			<div class="top-nav"><?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?></div>
+			<div class="top-nav">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</div>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
