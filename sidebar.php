6795defb6a5620d976230fbef263e4d884a56f21
Lena Plaksina <lena.plaksina@gmail.com>
2015-09-25 10:39:07 +1200

    Remove sidebar

--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,11 @@
  * @package lena_fttl
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
+// if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	// return;
+// } 
 ?>
 
-<div id="secondary" class="widget-area" role="complementary">
+<!-- <div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</div><!-- #secondary -->
+</div> #secondary -->
